68a0fd682a217f11db64365f31f5b07f274e898d
spushkaraj <spushkaraj.dev@gmail.com>
2023-12-01 17:51:36 +0530

    added solution for 328

---
+++ 328 - Odd Even Linked List/solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ */
+class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null)  return null;
+
+        if(head.next == null) return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while(even != null && even.next != null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+
+        return head;
+    }
+}
